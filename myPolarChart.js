7792bacebf726ef92cd069a42a3b3e36ad1d595d
KHYATI SHARMA <khyatisharma2001514@gmail.com>
2021-04-26 15:52:57 +0530

    custome range sliders

--- myPolarChart.js
+++ myPolarChart.js
@@ -141,23 +141,43 @@ $("#renderBtn").click(
 		renderChart(data, App.labels);
 	}
 );
+jQuery(document).ready(function($) {
+    $('.range-slider').each(function(index, el) {
+        $(this).append('<span class="range-number"><span>0</span></span>');
+    });
+    $('.range-slider').on('mousedown touchstart', '.range-number', function(event) {
+        event.preventDefault();
+        var $ele = $(this);
+        var $eleParent = $(this).parents('.range-slider');
+        var eleWid = $ele.innerWidth();
+        var eleOffset = 1;
+        var thisEnd = $eleParent.data('end');
+        var parentWid = $eleParent.innerWidth();
+        console.log(event, $ele, $eleParent);
+        $eleParent.addClass('tap');
+        $eleParent.on('mousemove touchmove', function(event) {
+            event.preventDefault();
+            var leftOff = event.offsetX-(eleWid/2);
+            var leftSpc = (leftOff*100)/$eleParent[0].offsetWidth;
+            var leftText = (leftOff + eleOffset * 2) - eleOffset;
+            if(leftOff <= (parentWid-eleWid-eleOffset) && leftOff >= eleOffset){
+                var temp = parentWid-eleWid-(eleOffset*2);
+                var temp1 = Math.floor(((leftOff-eleOffset)/temp)*thisEnd);
+                $ele.css('left', leftSpc+'%');
+                $ele.find('span').text(temp1);
+            }
+        });
+    });
+    $('.range-slider').on('mouseup touchend', function(event) {
+        event.preventDefault();
+        var $ele = $(this);
+        var $eleParent = $(this).parents('.range-slider');
+        $ele.removeClass('tap');
+        $ele.off('mousemove touchmove');
+    });
+});
 
-var rangeSlider = function () {
-	var slider = $('.row'),
-		range = $('.form-range'),
-		value = $('.slider-value');
 
-	slider.each(function () {
 
-		value.each(function () {
-			var value = $(this).prev().attr('value');
-			$(this).html(value);
-		});
 
-		range.on('input', function () {
-			$(this).next(value).html(this.value);
-		});
-	});
-};
 
-rangeSlider();
